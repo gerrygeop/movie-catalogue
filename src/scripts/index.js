39e6e444bdcb011209ff2e8f8550a380ed8f9639
gerrygeop <gerryja28@gmail.com>
2021-05-18 04:11:06 +0800

    Create simple URL router

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,4 +9,12 @@ const app = new App({
     content: document.getElementById('mainContent')
 });
 
+window.addEventListener('hashchange', () => {
+    app.renderPage();
+});
+
+window.addEventListener('load', () => {
+    app.renderPage();
+})
+
 console.log('Hello Geopers!');
