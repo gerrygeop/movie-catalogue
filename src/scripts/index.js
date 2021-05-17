1029fd15ad8feb0f6264dcfe3f01f656dedfa882
gerrygeop <gerryja28@gmail.com>
2021-05-18 03:27:32 +0800

    Add Navigation Drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,12 @@
 import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
+import App from './views/app';
+
+const app = new App({
+    button: document.getElementById('hamburgerButton'),
+    drawer: document.getElementById('navigationDrawer'),
+    content: document.getElementById('mainContent')
+});
 
 console.log('Hello Geopers!');
